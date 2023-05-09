74fcfcc8a6270464da68375bbb56071542e3f121
EricoBruner <ericodasilvabruner@gmail.com>
2023-05-09 19:33:25 -0300

    remoção de arquivos não usados e criação da estrutura de pastas

---
+++ src/components/Game/index.js
@@ -0,0 +1,14 @@
+import styles from "./styles.module.css";
+
+export default function Game() {
+  return (
+    <>
+      <img src="assets/forca0"></img>
+
+      <div>
+        <button></button>
+        <div>_ _ _ _ _ _ _</div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Letters/index.js
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+import alfabeto from "./alfabeto";
+
+export default function Letters() {
+  return <h1>Letters</h1>;
+}
