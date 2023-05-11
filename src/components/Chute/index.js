69eb9858c2962e68c27cb9e7ee8af8297e41a178
EricoBruner <ericodasilvabruner@gmail.com>
2023-05-11 18:57:02 -0300

    adicionando data-test

--- src/components/Chute/index.js
+++ src/components/Chute/index.js
@@ -8,11 +8,13 @@ export default function Chute({ chutandoPalavra }) {
     <div className={styles.container}>
       <h1>Já sei a palavra!</h1>
       <input
+        data-test="guess-input"
         type="text"
         value={chute}
         onChange={(e) => setChute(e.target.value)}
       />
       <button
+        data-test="guess-button"
         onClick={() => {
           chutandoPalavra(chute);
         }}
